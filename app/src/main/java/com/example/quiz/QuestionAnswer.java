ee5ecc5d91f0f2a289714dbdc427e94190dd78b4
shivang singh <shivangsinghblw@gmail.com>
2023-12-10 17:37:47 +0530

    SIMPLE QUIZ APP CODE DEMONSTRATION

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,19 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static String question[]={
+           "Which company owns android?",
+           "Which one of the following is not an programming language?",
+           "what language you can make best use for developing apps?"
+    };
+    public static String choices[][]={
+            {"Google","Apple","Noikia","Samsung"},
+            {"Java","Kotlin","HTML","Python"},
+            {"Java","Kotlin","java script","Php"}
+    };
+    public static String correctAnswers[]={
+            "Google",
+            "HTML",
+            "Kotlin"
+    };
+}
